3592e82e0fd2665aaab9962b620dbdb166cdac7f
ShrikeBin <grahamka.ha@gmail.com>
2026-04-06 22:22:15 +0200

    need lotta time and mst fixes

--- MH/list1/run/run.cpp
+++ MH/list1/run/run.cpp
@@ -1,19 +1,16 @@
 #include "utils.hpp"
 
+#include <filesystem>
+#include <chrono>
+
 // Global random generator
 std::random_device RD;
 std::mt19937 GEN(RD());
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <tsplib_file>\n";
-        return 1;
-    }
-
-    std::vector<City> cities = read_data(argv[1]);
+void run_for_file(const std::vector<City>& cities, const std::string& filename) {
     int n = cities.size();
 
-    std::cout << "Loaded file: " << argv[1] << " | Number of vertices (n): " << n << "\n\n";
+    std::cout << "Loaded file: " << filename << " | Number of vertices (n): " << n << "\n\n";
 
     std::vector<std::vector<uint_fast32_t>> dist_matrix(n, std::vector<uint_fast32_t>(n));
     for (uint_fast32_t i = 0; i < n; ++i) {
@@ -22,13 +19,12 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    std::cout << "TASK 1:\n";
-
     long long sum_dist_1 = 0;
     long long sum_steps_1 = 0;
     int best_dist_1 = std::numeric_limits<int>::max();
 
     for (uint_fast32_t k = 0; k < n; ++k) {
+        std::cout<< "Iteration " << k + 1 << "/" << n << "\r" << std::flush;
         std::vector<uint_fast32_t> initial_tour = random_tour(n, GEN);
         LocalSearchResult result = local_search_naive(initial_tour, dist_matrix);
         
@@ -37,11 +33,10 @@ int main(int argc, char* argv[]) {
         if (result.distance < best_dist_1) best_dist_1 = result.distance;
     }
 
+    std::cout << "TASK 1:\n";
     std::cout << "Average solution value: " << static_cast<double>(sum_dist_1) / n << "\n";
     std::cout << "Average improvement steps: " << static_cast<double>(sum_steps_1) / n << "\n";
     std::cout << "Best solution: " << best_dist_1 << "\n\n";
-
-    std::cout << "TASK 2:\n";
 
     long long sum_dist_2 = 0;
     long long sum_steps_2 = 0;
@@ -56,22 +51,18 @@ int main(int argc, char* argv[]) {
         if (result.distance < best_dist_2) best_dist_2 = result.distance;
     }
 
+    std::cout << "TASK 2:\n";
     std::cout << "Average solution value: " << static_cast<double>(sum_dist_2) / n << "\n";
     std::cout << "Average improvement steps: " << static_cast<double>(sum_steps_2) / n << "\n";
     std::cout << "Best solution: " << best_dist_2 << "\n\n";
-
-    int iterations = std::ceil(std::sqrt(n));   // sufit z pierwiastka z n
-    std::cout << "TASK 3: \n";
-
-    // liczenie mst i jego wagi
-    MSTresult mst_result = prim_mst(cities, dist_matrix);
-    std::cout << "Weight of the Minimum Spanning Tree (MST): " << mst_result.total_weight << "\n";
 
     long long sum_dist_3 = 0;
     long long sum_steps_3 = 0;
     int best_dist_3 = std::numeric_limits<int>::max();
 
     std::uniform_int_distribution<> start_vertex_dist(1, n);
+    int iterations = std::ceil(std::sqrt(n));
+    MSTresult mst_result = prim_mst(cities, dist_matrix);
 
     for (int k = 0; k < iterations; ++k) {
         // Losowy wierzchołek startowy dla DFS
@@ -88,9 +79,57 @@ int main(int argc, char* argv[]) {
         if (result.distance < best_dist_3) best_dist_3 = result.distance;
     }
 
+    std::cout << "TASK 3: \n";
+    std::cout << "Weight of the Minimum Spanning Tree (MST): " << mst_result.total_weight << "\n";
     std::cout << "Average solution value: " << static_cast<double>(sum_dist_3) / iterations << "\n";
     std::cout << "Average improvement steps: " << static_cast<double>(sum_steps_3) / iterations << "\n";
     std::cout << "Best solution: " << best_dist_3 << "\n";
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <file/directory>\n";
+        return 1;
+    }
+
+    std::vector<std::string> file_queue;
+
+    for (int i = 1; i < argc; ++i) {
+        std::filesystem::path p(argv[i]);
+
+        if (std::filesystem::is_directory(p)) {
+            for (const auto& entry : std::filesystem::directory_iterator(p)) {
+                if (entry.path().extension() == ".tsp") {
+                    file_queue.push_back(entry.path().string());
+                }
+            }
+        } else if (std::filesystem::exists(p)) {
+            file_queue.push_back(p.string());
+        } else {
+            std::cerr << "Warning: Path not found: " << argv[i] << "\n";
+        }
+    }
+
+    for (const std::string& filename : file_queue) {
+        
+        std::vector<City> cities = read_data(filename);
+        
+        if (cities.empty()) {
+            std::cerr << "Failed to load: " << filename << "\n";
+            continue;
+        }
+
+        std::cout << "------------------------------------------\n";
+        auto start = std::chrono::high_resolution_clock::now();
+
+        run_for_file(cities, filename);
+
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> duration = end - start;
+        std::cout << "\n";
+        std::cout << "Finished " << filename << " in " << duration.count() << " seconds.\n";
+        std::cout << "------------------------------------------\n";
+    }
 
     return 0;
 }
--- MH/list1/run/utils.cpp
+++ MH/list1/run/utils.cpp
@@ -25,16 +25,14 @@ std::vector<City> read_data(const std::string& filename) {
             if (iss >> new_city.id >> new_city.x >> new_city.y) {
                 cities.push_back(new_city);
             }
-        } else {
-            std::cerr << "Error parsing line: " << line << "\n";
         }
     }
     return cities;
 }
 
-std::vector<uint_fast32_t> random_tour(int num_cities, std::mt19937& GEN){
+std::vector<uint_fast32_t> random_tour(int num_cities, std::mt19937& GEN) {
     std::vector<uint_fast32_t> tour(num_cities);
-    std::iota(tour.begin(), tour.end(), 1);
+    std::iota(tour.begin(), tour.end(), 0);
     std::shuffle(tour.begin(), tour.end(), GEN);
     return tour;
 }
@@ -59,15 +57,15 @@ int_fast32_t diff_after_invert(const std::vector<uint_fast32_t>& tour, uint_fast
     // We invert around indexes(i, j) corresponding to cities a and b.
     uint_fast32_t n = tour.size();
 
-    if (i == 0 && j == n - 1 || i == j || i == n - 1 && j == 0) return 0;
+    if ((i == 0 && j == n - 1) || i == j || (i == n - 1 && j == 0)) return 0;
 
     uint_fast32_t prev_a_idx = (i == 0) ? n - 1 : i - 1;
     uint_fast32_t next_b_idx = (j == n - 1) ? 0 : j + 1;
 
-    uint_fast32_t a = tour[i] - 1;
-    uint_fast32_t b = tour[j] - 1;
-    uint_fast32_t prev_a = tour[prev_a_idx] - 1;
-    uint_fast32_t next_b = tour[next_b_idx] - 1;
+    uint_fast32_t a = tour[i];
+    uint_fast32_t b = tour[j];
+    uint_fast32_t prev_a = tour[prev_a_idx];
+    uint_fast32_t next_b = tour[next_b_idx];
 
     uint_fast32_t old_distance = dist_matrix[prev_a][a] + dist_matrix[b][next_b];
     uint_fast32_t new_distance = dist_matrix[prev_a][b] + dist_matrix[a][next_b];
@@ -81,14 +79,13 @@ LocalSearchResult local_search_naive(std::vector<uint_fast32_t> initial_tour, co
     uint_fast32_t current_distance = initial_distance;
 
     bool improvement = true;
-
     while(improvement) {
+        std::cout<< "Improvement step " << improvement_steps + 1 << " current distance: " << current_distance << "\n" << std::flush;
         improvement = false;
 
         int_fast32_t best_difference = 0;
         uint_fast32_t best_i = -1;
         uint_fast32_t best_j = -1;
-
         for(uint_fast32_t i = 0; i < initial_tour.size() - 1; ++i) {
             for(uint_fast32_t j = i + 1; j < initial_tour.size(); ++j) {
                 int_fast32_t difference = diff_after_invert(initial_tour, i, j, dist_matrix);
@@ -99,6 +96,7 @@ LocalSearchResult local_search_naive(std::vector<uint_fast32_t> initial_tour, co
                 }
             }
         }
+
         if(best_difference < 0) {
             std::reverse(initial_tour.begin() + best_i, initial_tour.begin() + best_j + 1); 
             current_distance += best_difference;
@@ -110,51 +108,46 @@ LocalSearchResult local_search_naive(std::vector<uint_fast32_t> initial_tour, co
 }
 
 LocalSearchResult local_search_n_random(std::vector<uint_fast32_t> initial_tour, const std::vector<std::vector<uint_fast32_t>>& dist_matrix, std::mt19937& GEN) {
-  uint_fast32_t improvement_steps = 0;
-  uint_fast32_t initial_distance = tour_distance(initial_tour, dist_matrix);
-  uint_fast32_t current_distance = initial_distance;
-  uint_fast32_t n = initial_tour.size();
+    uint_fast32_t improvement_steps = 0;
+    uint_fast32_t initial_distance = tour_distance(initial_tour, dist_matrix);
+    uint_fast32_t current_distance = initial_distance;
+    uint_fast32_t n = initial_tour.size();
 
-  bool improvement = true;
-  std::uniform_int_distribution<> dist(0, n - 1);
+    bool improvement = true;
+    std::uniform_int_distribution<> dist(0, n - 1);
 
-  while(improvement) {
-    improvement = false;
+    while(improvement) {
+        improvement = false;
 
-    int_fast32_t best_difference = 0;
-    uint_fast32_t best_i = -1;
-    uint_fast32_t best_j = -1;
+        int_fast32_t best_difference = 0;
+        uint_fast32_t best_i = -1;
+        uint_fast32_t best_j = -1;
 
-    for(uint_fast32_t k = 0; k < n; ++k) {
-        uint_fast32_t i = dist(GEN);
-        uint_fast32_t j = dist(GEN);
-        while (j == i) j = dist(GEN);
-        if (i > j) std::swap(i, j);
+        for(uint_fast32_t k = 0; k < n; ++k) {
+            uint_fast32_t i = dist(GEN);
+            uint_fast32_t j = dist(GEN);
+            while (j == i) j = dist(GEN);
+            if (i > j) std::swap(i, j);
 
-        int_fast32_t difference = diff_after_invert(initial_tour, i, j, dist_matrix);
-        if (difference < best_difference) {
-            best_difference = difference;
-            best_i = i;
-            best_j = j;
+            int_fast32_t difference = diff_after_invert(initial_tour, i, j, dist_matrix);
+            if (difference < best_difference) {
+                best_difference = difference;
+                best_i = i;
+                best_j = j;
+            }
+        }
+
+        if(best_difference < 0) {
+            std::reverse(initial_tour.begin() + best_i, initial_tour.begin() + best_j + 1); 
+            current_distance += best_difference;
+            improvement_steps++;
+            improvement = true;
         }
     }
-
-    if(best_difference < 0) {
-        std::reverse(initial_tour.begin() + best_i, initial_tour.begin() + best_j + 1); 
-        current_distance += best_difference;
-        improvement_steps++;
-        improvement = true;
-    }
-  }
-  return LocalSearchResult{initial_tour, current_distance, improvement_steps};
+    return LocalSearchResult{initial_tour, current_distance, improvement_steps};
 }
 
-
-
-
-
-
-///TODO: FIX THOSE
+///TODO: FIX THOSE TO BE FASTER
 MSTresult prim_mst(const std::vector<City>& cities, const std::vector<std::vector<uint_fast32_t>>& dist_matrix) {
     int n = cities.size();
     
@@ -167,8 +160,8 @@ MSTresult prim_mst(const std::vector<City>& cities, const std::vector<std::vecto
     // -1 brak rodzica
     std::vector<int> parent(n, -1); 
 
-    std::vector<std::pair<uint_fast32_t, uint_fast32_t>> edges;
-    uint_fast32_t total_weight = 0;
+    std::vector<std::pair<int, int>> edges;
+    int total_weight = 0;
 
     min_dist[0] = 0;
 
@@ -187,7 +180,7 @@ MSTresult prim_mst(const std::vector<City>& cities, const std::vector<std::vecto
         visited[u] = true;    // już odwiedzony
         
         if (parent[u] != -1) {
-          // zapisanie id miast zamiast indeksów
+            // zapisanie id miast zamiast indeksów
             edges.push_back({cities[parent[u]].id, cities[u].id});
             total_weight += current_min;
         }
@@ -203,10 +196,11 @@ MSTresult prim_mst(const std::vector<City>& cities, const std::vector<std::vecto
             }
         }
     }
+
     return {edges, total_weight};
 }
 
-void dfs(int u, const std::vector<std::vector<uint_fast32_t>>& adj, std::vector<bool>& visited, std::vector<uint_fast32_t>& tour) {
+void dfs(int u, const std::vector<std::vector<int>>& adj, std::vector<bool>& visited, std::vector<uint_fast32_t>& tour) {
     visited[u] = true;
     tour.push_back(u);
     
@@ -217,21 +211,21 @@ void dfs(int u, const std::vector<std::vector<uint_fast32_t>>& adj, std::vector<
     }
 }
 
-std::vector<int> mst_to_tsp(const std::vector<std::pair<int, int>>& mst_edges, int num_vertices, int start_vertex) {
+std::vector<uint_fast32_t> mst_to_tsp(const std::vector<std::pair<int, int>>& mst_edges, int num_vertices, int start_vertex) {
 
     std::vector<std::vector<int>> adj(num_vertices + 1);
     
     // lista sąsiedztwa dla drzewa MST
     for (const auto& edge : mst_edges) {
-        adj[edge.first].push_back(edge.second);
-        adj[edge.second].push_back(edge.first);
+        adj[edge.first-1].push_back(edge.second-1);
+        adj[edge.second-1].push_back(edge.first-1);
     }
 
     std::vector<bool> visited(num_vertices + 1, false);
-    std::vector<int> initial_tour;
+    std::vector<uint_fast32_t> initial_tour;
     
     // konstruowanie TSP poprzez DFS po drzewie MST poczynając od start_vertex
     dfs(start_vertex, adj, visited, initial_tour);
-    
+
     return initial_tour;
 }
