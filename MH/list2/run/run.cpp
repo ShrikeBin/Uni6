d2b967771cbc924029d6f8be9ef11d12c735e6eb
ShrikeBin <grahamka.ha@gmail.com>
2026-05-09 14:17:26 +0200

    out ready i guess

--- MH/list2/run/run.cpp
+++ MH/list2/run/run.cpp
@@ -11,7 +11,7 @@
 std::random_device RD;
 std::mt19937 GEN(RD());
 
-void run_for_file(const std::vector<City>& cities, const std::string& filename) {
+void run_exp_for_file(const std::vector<City>& cities, const std::string& filename) {
     bool PRINT_TOURS = false;
     bool PRINT_BEST_ONLY = true;
     bool DEBUG_ITER = false;
@@ -32,8 +32,8 @@ void run_for_file(const std::vector<City>& cities, const std::string& filename) 
     // Taboo experiments
     uint_fast32_t TABU_REPETITIONS = 30;
     std::vector<uint_fast32_t> cooldowns = {5, 10, static_cast<uint_fast32_t>(std::log(n)), static_cast<uint_fast32_t>(std::sqrt(n))};
-    std::vector<uint_fast32_t> max_iters = {static_cast<uint_fast32_t>(std::sqrt(n)), 100, 1000, 4000};
-    std::vector<uint_fast32_t> sample_sizes = {static_cast<uint_fast32_t>(std::sqrt(n)), n};
+    std::vector<uint_fast32_t> max_iters = {static_cast<uint_fast32_t>(std::sqrt(n)), 100, 1000, n / 2};
+    std::vector<uint_fast32_t> sample_sizes = {static_cast<uint_fast32_t>(std::sqrt(n)), n / 2};
 
     uint_fast32_t bestAVGTabu = -1;
     TabuResult bestTabuConfig;
@@ -107,8 +107,8 @@ void run_for_file(const std::vector<City>& cities, const std::string& filename) 
     uint_fast32_t ANNEALING_REPETITIONS = 30;
     std::vector<double> init_temps = {1000, static_cast<double>(n), static_cast<double>(2*n), static_cast<double>(10*n)};
     std::vector<double> cooling_rates = {0.8, 0.85, 0.9, 0.95, 0.99};
-    std::vector<uint_fast32_t> epoch_lengths = {5, 10, static_cast<uint_fast32_t>(std::log(n)), static_cast<uint_fast32_t>(std::sqrt(n))};
-    std::vector<uint_fast32_t> max_epochs = {static_cast<uint_fast32_t>(std::sqrt(n)), 100, n, 5*n, 10*n, 30*n};
+    std::vector<uint_fast32_t> epoch_lengths = {5, 10, static_cast<uint_fast32_t>(std::log(n)), static_cast<uint_fast32_t>(std::sqrt(n)), n / 2};
+    std::vector<uint_fast32_t> max_epochs = {static_cast<uint_fast32_t>(std::sqrt(n)), 100, n, 5*n};
 
     uint_fast32_t bestAVGAnnealing = -1;
     SAResult bestAnnealingConfig;
@@ -180,6 +180,129 @@ void run_for_file(const std::vector<City>& cities, const std::string& filename) 
     "Init Temp: "                       << bestAnnealingConfig.init_temp                << "\n\n";
 }
 
+void run_best_for_file(const std::vector<City>& cities, const std::string& filename) {
+    bool DEBUG_ITER = true;
+    unsigned int n = cities.size();
+
+    std::cout << "Loaded file: " << filename << " | Number of vertices (n): " << n << "\n\n";
+
+
+    std::vector<std::vector<uint_fast32_t>> dist_matrix(n, std::vector<uint_fast32_t>(n));
+    for (uint_fast32_t i = 0; i < n; ++i) {
+        for (uint_fast32_t j = 0; j < n; ++j) {
+            dist_matrix[i][j] = discrete_distance(cities[i], cities[j]);
+        }
+    }
+
+    uint_fast32_t TABU_REPETITIONS = 50;
+    std::vector<uint_fast32_t> cooldowns = {15};
+    std::vector<uint_fast32_t> max_iters = {n / 2};
+    std::vector<uint_fast32_t> sample_sizes = {n};
+
+    uint_fast32_t bestAVGTabu = -1;
+    TabuResult bestTabuConfig;
+
+    for(auto cooldown: cooldowns) {
+        for(auto max_iter : max_iters) {
+            for(auto sample_size : sample_sizes) {
+                std::vector<uint_fast32_t> best_tour_taboo = random_tour(n, GEN);
+                uint_fast32_t best_distance = -1;
+                uint_fast64_t sum_distance = 0;
+                uint_fast64_t sum_iters = 0;
+
+                std::cout << "\n====================[TABU]=====================\n" <<
+                    "City: "                            << filename     << 
+                    " (" << n << ")"                    << "\n"
+                    "Max Iterations w/no improvement: " << max_iter     << "\n" 
+                    "Tabu list expire cooldown: "       << cooldown     << "\n"
+                    "Sample size for 2-opt: "           << sample_size  << "\n\n";
+
+                for(uint_fast32_t i = 1; i <= TABU_REPETITIONS; ++i){
+                    std::cout << std::flush;
+                    TabuResult result = tabu_search(random_tour(n, GEN), dist_matrix, GEN, cooldown, max_iter, sample_size);
+
+                    if(DEBUG_ITER) {
+                        std::cout<< "Iteration " << i << "/" << TABU_REPETITIONS << " Dist: - " << result.distance << " - \r" << std::flush;
+                    }
+                    std::cout << std::flush;
+
+                    sum_distance += result.distance;
+                    sum_iters += result.iters;
+
+                    if(result.distance < best_distance) {
+                        best_tour_taboo = result.tour;
+                        best_distance = result.distance;
+                    }
+                }
+                double AVG = static_cast<double>(sum_distance) / TABU_REPETITIONS;
+
+                std::cout << "Average solution value: "     << AVG                                                  << "\n";
+                std::cout << "Average improvement steps: "  << static_cast<double>(sum_iters)    / TABU_REPETITIONS << "\n";
+                std::cout << "Best solution: "              << best_distance                                        << "\n";
+                std::cout << "\n====================[TOUR]=====================\n";
+                for(uint_fast32_t city_id : best_tour_taboo) {
+                    std::cout << city_id << ">";
+                }
+            }
+        }
+    }
+    
+    uint_fast32_t ANNEALING_REPETITIONS = 50;
+    std::vector<double> init_temps = {static_cast<double>(10*n)};
+    std::vector<double> cooling_rates = {0.95};
+    std::vector<uint_fast32_t> epoch_lengths = {n / 2};
+    std::vector<uint_fast32_t> max_epochs = {n / 2};
+
+    uint_fast32_t bestAVGAnnealing = -1;
+    SAResult bestAnnealingConfig;
+
+    for(auto init_temp: init_temps) {
+        for(auto cooling_rate : cooling_rates) {
+            for(auto epoch_lenght : epoch_lengths){
+                for(auto max_epoch : max_epochs) {
+                    std::vector<uint_fast32_t> best_tour_annealing = random_tour(n, GEN);
+                    uint_fast32_t best_distance = -1;
+                    uint_fast64_t sum_distance = 0;
+
+                    std::cout << "\n====================[ANNEALING]=====================\n" <<
+                        "City: "                            << filename     << 
+                        " (" << n << ")"                    << "\n"
+                        "Max epochs w/no improvement: "     << max_epoch    << "\n" 
+                        "Tries per epoch: "                 << epoch_lenght << "\n"
+                        "Cooling rate: "                    << cooling_rate << "\n"
+                        "Init Temp: "                       << init_temp    << "\n\n";
+
+                    for(uint_fast32_t i = 1; i <= ANNEALING_REPETITIONS; ++i) {
+                        std::cout << std::flush;
+
+                        SAResult result = simulated_annealing(random_tour(n, GEN), dist_matrix, GEN, init_temp, cooling_rate, epoch_lenght, max_epoch);
+
+                        if(DEBUG_ITER) {
+                            std::cout<< "Iteration " << i << "/" << TABU_REPETITIONS << " Dist: - " << result.distance << " - \r" << std::flush;
+                        }
+                        std::cout << std::flush;
+                        
+                        sum_distance += result.distance;
+
+                        if(result.distance < best_distance) {
+                            best_tour_annealing = result.tour;
+                            best_distance = result.distance;
+                        }
+                    }
+                    double AVG = static_cast<double>(sum_distance) / ANNEALING_REPETITIONS;
+
+                    std::cout << "Average solution value: "     << AVG              << "\n";
+                    std::cout << "Best solution: "              << best_distance    << "\n";
+                    std::cout << "\n====================[TOUR]=====================\n";
+                    for(uint_fast32_t city_id : best_tour_annealing) {
+                        std::cout << city_id << ">";
+                    }
+                }
+            }
+        }
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " <file/directory>\n";
@@ -217,7 +340,10 @@ int main(int argc, char* argv[]) {
         std::cout << "------------------------------------------\n";
         auto start = std::chrono::high_resolution_clock::now();
 
-        run_for_file(cities, filename);
+        {
+            // run_exp_for_file(cities, filename);
+            run_best_for_file(cities, filename);
+        }
 
         auto end = std::chrono::high_resolution_clock::now();
         std::chrono::duration<double> duration = end - start;
